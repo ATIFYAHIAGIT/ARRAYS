cf5598dd33657150aabd82b74700ef71a551ee0d
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-21 22:48:34 +0530

    selection sort

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+int main () {
+    int arr[5]={232,6,857,4,57},index;
+
+    for (int i=0;i<4;i++) {
+        index=i;
+        for(int j=i+1; j<5; j++) {
+            if (arr[index]>arr[j]) {
+                index=j;
+            }
+        }
+        swap(arr[i],arr[index]);
+    }
+
+    for (int i=0;i<5;i++) {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

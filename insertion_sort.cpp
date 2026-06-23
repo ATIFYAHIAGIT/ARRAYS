dfa3accb679c8486fced7312b1eb8c5248f9eb5a
ATIFYAHIAGIT <atifyahia99@gmail.com>
2026-06-23 12:14:09 +0530

    insertiond sort done

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+void array(int arr[], int x) {
+    for(int i=0;i<x;i++) {
+        cout<<"Enter the element "<<(i+1)<<": ";
+        cin>>arr[i];
+        cout<<"\n";
+    }
+    cout<<"The elements of the array are: ";
+    for(int i=0;i<x;i++) {
+        cout<<arr[i]<<" ";
+    }
+    return;
+}
+
+void insertion_sort(int arr[], int x) {
+    for(int i=1;i<x;i++) {
+        for(int j=i;j>0;j--) {
+                if(arr[j] > arr [j-1]){
+                    arr[j]=arr[j] + arr [j-1];
+                    arr[j-1]=arr[j] - arr [j-1];
+                    arr[j]=arr[j] - arr[j-1];;
+               }
+        }
+    }
+
+    cout<<"The sorted array is: ";
+    for(int i=0;i<x;i++) {
+        cout<<arr[i]<<" ";
+    }
+    return;
+}
+
+int main () {
+    int x;
+    cout<<"Enter the number of elements ot the array: ";
+    cin>>x;
+    
+    int arr[x];
+    array(arr,x);
+    cout<<"\n";
+    insertion_sort(arr,x);
+    return 0;
+}
